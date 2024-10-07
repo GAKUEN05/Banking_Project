a19bd671fb1dedd1486544dff6dccecc70225767
netbook <tchegou1996@gmail.com>
2024-10-07 22:42:32 +0200

    create tables and connect to DB

--- system.cpp
+++ system.cpp
@@ -7,78 +7,83 @@
 #include <cmath>
 vector<customer> client;
 void write_into_file()
+{
+    string file_name;
+    cout << "Please could you enter the name of the file in which you want to write : ";
+    cin >> file_name;
+    ofstream outFile(file_name);
+    int choice1;
+    int choice2 = 1;
+    cout << "||FILLING OF THE CLIENTS INFORMATION|| " << endl;
+    cout << "Do you want to add a client ? " << " Yes (then press 1), No (then press 2) " << endl;
+    cin >> choice1;
+    switch (choice1)
     {
-        string file_name;
-        cout << "Please could you enter the name of the file in which you want to write : ";
-        cin >> file_name;
-        ofstream outFile(file_name);
-        int choice1;
-        int choice2=1;
-        cout << "||FILLING OF THE CLIENTS INFORMATION|| " << endl;
-        cout << "Do you want to add a client ? "<<" Yes (then press 1), No (then press 2) " << endl;
-        cin >> choice1;
-        switch(choice1)
+    case 1:
+        if (outFile.is_open())
+        {
+            do
             {
-                case 1:
-                    if (outFile.is_open())
-                        {
-                            do{
-                                string first_name;
-                                string last_name;
-                                srand(time(0));
-                                int id=rand() % 100001;
-                                cout <<"Enter the first_name : ";
-                                cin >> first_name;
-                                cout <<"Enter the last_name : ";
-                                cin >> last_name;
-                                customer* dear_client=new customer(id,first_name,last_name);
-                                int client_number=rand() % 100001;
-                                double montant_client;
-                                double rate_of_client=double(rand()) / RAND_MAX * 0.9;
-                                rate_of_client=round(rate_of_client*1000)/1000;
-                                cout <<"How much do you want to put in this account ? ";
-                                cin >> montant_client;
-                                account* dear_account= new account(client_number,montant_client,rate_of_client);
-                                dear_client->add_account(dear_account);
-                                client.push_back(*dear_client);
-                                outFile <<"Client ID : "<<id << ", " <<"Name : " <<first_name<< " " << last_name <<", account_id : "<<client_number<<", balance : "<< montant_client <<" €"<<", interest rate: "<< rate_of_client<<"\n";
-                                cout << "Do you want to add another client ? "<<" Yes (then press 1), No (then press 2) " << endl;
-                                cin >> choice2;
-                            }while(choice2==1);
-                            cout << "Ok, Have a nice day :)" << endl;
-                            outFile.close();
-                            break;
-                        }
-                    else
-                        {
-                            cerr << "Unable to open file for writing\n";
-                            break;
-                        }
-                case 2:
-                cout << "Ok, Have a nice day :)" << endl;
-                break;
-                default:
-                    throw :: invalid_argument("You haven't follow the instruction  !!!");
-                    }
-        
-    };
+                string first_name;
+                string last_name;
+                srand(time(0));
+                int id = rand() % 100001;
+                cout << "Enter the first_name : ";
+                cin >> first_name;
+                cout << "Enter the last_name : ";
+                cin >> last_name;
+                customer *dear_client = new customer(id, first_name, last_name);
+                int client_number = rand() % 100001;
+                double montant_client;
+                double rate_of_client = double(rand()) / RAND_MAX * 0.9;
+                rate_of_client = round(rate_of_client * 1000) / 1000;
+                cout << "How much do you want to put in this account ? ";
+                cin >> montant_client;
+                account *dear_account = new account(client_number, montant_client, rate_of_client);
+                dear_client->add_account(dear_account);
+                client.push_back(*dear_client);
+                outFile << "Client ID : " << id << ", " << "Name : " << first_name << " " << last_name << ", account_id : " << client_number << ", balance : " << montant_client << " €" << ", interest rate: " << rate_of_client << "\n";
+                cout << "Do you want to add another client ? " << " Yes (then press 1), No (then press 2) " << endl;
+                cin >> choice2;
+            } while (choice2 == 1);
+            cout << "Ok, Have a nice day :)" << endl;
+            outFile.close();
+            break;
+        }
+        else
+        {
+            cerr << "Unable to open file for writing\n";
+            break;
+        }
+    case 2:
+        cout << "Ok, Have a nice day :)" << endl;
+        break;
+    default:
+        throw ::invalid_argument("You haven't follow the instruction  !!!");
+    }
+};
 void read_into_file()
+{
+    string file_name;
+    cout << "Please could you enter the name of the file you're looking for :";
+    cin >> file_name;
+    ifstream inFile(file_name);
+    string line;
+    if (inFile.is_open())
     {
-        string file_name;
-        cout << "Please could you enter the name of the file you're looking for :";
-        cin >> file_name;
-        ifstream inFile(file_name);
-        string line;
-        if (inFile.is_open()) {
-            while (getline(inFile, line)) {
-                cout << line << endl;
-            }
+        while (getline(inFile, line))
+        {
+            cout << line << endl;
+        }
         inFile.close();
-    } else {
-            cerr << "Unable to open file for reading\n";
     }
+    else
+    {
+        cerr << "Unable to open file for reading\n";
     }
-int main(){
+}
+int main()
+{
     write_into_file();
     read_into_file();
     // //** Print information about account **//
@@ -107,7 +112,7 @@ int main(){
     //** Testing information about transaction **/
     // transaction paiement(258,"03-25-2024",withdrawals,1258);
     // yami.client_account[0].add_transaction(&paiement,yami.client_account[0].getaccountId());
-    // yami.print_transaction_info(); //Print all transaction (I mean all information related to transactions) in an account 
+    // yami.print_transaction_info(); //Print all transaction (I mean all information related to transactions) in an account
     // cout << "Transaction ID : "<< yami.client_account[0].client_transaction[0]->getTransactionId() <<endl;
     // cout << "Date of the transaction : "<< yami.client_account[0].client_transaction[0]->getDateOfTransaction() <<endl;
     // cout << "Type of transaction : "  << yami.client_account[0].client_transaction[0]->getTransactionType() <<endl;
